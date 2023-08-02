cd2f601c51b31097b2c41d3ab52ef745cae9143c
nguyentientruong0201 <84010674+nguyentientruong0201@users.noreply.github.com>
2023-08-03 03:17:14 +0700

    Authentication

--- database/migrations/2023_07_26_082204_create_categories_table.php
+++ database/migrations/2023_07_26_082204_create_categories_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('slug')->unique();
+            $table->string('name', 255)->unique();
+            $table->string('slug', 255)->unique();
             $table->unsignedInteger('parent')->default(0);
             $table->unsignedInteger('created_by');
             $table->text('content')->nullable();
-            $table->string('meta_title')->nullable();
-            $table->string('meta_desc')->nullable();
-            $table->string('meta_keyword')->nullable();
+            $table->string('meta_tile', 255)->nullable();
+            $table->string('meta_desc', 255)->nullable();
+            $table->string('meta_keyword', 255)->nullable();
             $table->timestamps();
         });
     }
